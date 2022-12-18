2c5bda16def4be3b9780672036ed4ed0af783121
Kurnia Ramadhan Putra <kurniaramadhan@itenas.ac.id>
2022-12-19 04:20:54 +0700

    development feature persegi panjang and testing

--- AplikasiBangunDatar/src/main/java/com/itenas/aplikasibangundatar/MainApp.java
+++ AplikasiBangunDatar/src/main/java/com/itenas/aplikasibangundatar/MainApp.java
@@ -15,5 +15,12 @@ public class MainApp {
         persegi.setSisi(5);
         System.out.println("Luas Persegi: " + persegi.luas());
         System.out.println("Keliling Persegi: " + persegi.keliling());
+        
+        PersegiPanjang persegiPanjang = new PersegiPanjang();
+        persegiPanjang.setNamaBangunDatar("PersegiPanjang");
+        persegiPanjang.setPanjang(4);
+        persegiPanjang.setLebar(2);
+        System.out.println("Luas Persegi Panjang: " + persegiPanjang.luas());
+        System.out.println("Keliling Persegi Panjang: " + persegiPanjang.keliling());
     }
 }
---
+++ AplikasiBangunDatar/src/main/java/com/itenas/aplikasibangundatar/PersegiPanjang.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itenas.aplikasibangundatar;
+
+/**
+ *
+ * @author Kurnia R. Putra
+ */
+public class PersegiPanjang extends BangunDatar {
+    private double panjang;
+    private double lebar;
+
+    public PersegiPanjang() {
+    }
+
+    public double getPanjang() {
+        return panjang;
+    }
+
+    public void setPanjang(double panjang) {
+        this.panjang = panjang;
+    }
+
+    public double getLebar() {
+        return lebar;
+    }
+
+    public void setLebar(double lebar) {
+        this.lebar = lebar;
+    }
+
+    @Override
+    double luas() {
+        return panjang * lebar;
+    }
+
+    @Override
+    double keliling() {
+        return 2 * (panjang + lebar);
+    }
+}
